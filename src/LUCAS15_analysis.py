5b1d19faddef6d13ff9d858a77128c96a1010fc8
MMS <mms@Mehdis-MacBook-Pro.local>
2021-01-01 18:12:15 +0100

    argpars added

--- src/LUCAS15_analysis.py
+++ src/LUCAS15_analysis.py
@@ -29,12 +29,18 @@ _OUTPUT_PATH = "script-out"
 
 
 def parse_arg():
-    parser = argparse.ArgumentParser(description='Fully connected Neural Networks')
+    parser = argparse.ArgumentParser(prog='LUCAS15_analysis.py', description='Fully connected neural network for WORLDSOIL')
+    parser.add_argument("-i", "--input", dest='input', type=str, help="", required=True)
+    parser.add_argument("-o", "--output", dest='output', type=str, help="output filename ")
+    parser.add_argument("-b", "--batchSize", dest="batchSize", type= int, help="batch size value", default=20)
+    parser.add_argument("-e", "--epochs", dest="epochSize", type=int, help="epochs value", default=400)
+    parser.add_argument("-d", "--dropout", dest="dropOut", type=int, help="Dropout value", default=0.2)
+    parser.add_argument("-l", "--layer", dest="hiddenLayers", type=int, help="number of hidden layers", default=3)
 
-    return parser.parse_args()
+    return parser
 
 
-def Seperating_data_set(data_frame: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
+def separating_data_set(data_frame: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
     """
     Prepare the raw data by :
         - Seperating dataset to two cluster namely calibration and testing
@@ -57,8 +63,8 @@ def splitting_dataset(data_frame: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
         two pandas dataFrame: df_x and df_y
 
     """
-    data_frame_x = data_frame.iloc[:, 5:]
-    data_frame_y = data_frame.iloc[:, 4]
+    data_frame_x = data_frame.iloc[:, 6:]
+    data_frame_y = data_frame.iloc[:, 5]
 
     return data_frame_x, data_frame_y
 
@@ -71,20 +77,23 @@ def build_fnn(data_frame_x: pd.DataFrame):
         model
 
     """
+    parser = parse_arg()
+    args = parser.parse_args()
+
     fnn_model = tf.keras.models.Sequential()
     # input layer + first hidden layer
     fnn_model.add(tf.keras.layers.Dense(units=data_frame_x.shape[1],
                                         activation='relu',
                                         input_shape=[data_frame_x.shape[1]]))
-    fnn_model.add(tf.keras.layers.Dropout(0.2))
+    fnn_model.add(tf.keras.layers.Dropout(args.dropOut))
 
     # second hidden layer
     fnn_model.add(tf.keras.layers.Dense(units=data_frame_x.shape[1], activation='relu'))
-    fnn_model.add(tf.keras.layers.Dropout(0.2))
+    fnn_model.add(tf.keras.layers.Dropout(args.dropOut))
 
     # third hidden layer
     fnn_model.add(tf.keras.layers.Dense(units=data_frame_x.shape[1], activation='relu'))
-    fnn_model.add(tf.keras.layers.Dropout(0.2))
+    fnn_model.add(tf.keras.layers.Dropout(args.dropOut))
 
     fnn_model.add(tf.keras.layers.Dense(1))
 
@@ -145,36 +154,41 @@ def perform_analysis():
 
     :return:
     """
+    parser = parse_arg()
+    args = parser.parse_args()
 
-    if len(sys.argv) == 1:  # no arguments, so print help message
-        print("""Usage: python script.py data_path program_input out_path""")
-        return
+    # if len(sys.argv) == 1:  # no arguments, so print help message
+    #     print("""Usage: python script.py data_path program_input out_path""")
+    #     return
+    #
+    # dir_in = os.getcwd()
+    # dir_out = os.getcwd()
+    #
+    # try:
+    #     dir_in = sys.argv[1]
+    #     dir_out = sys.argv[2]
+    # except:
+    #     print("Parameters: path/to/simple/file  input/folder  output/folder")
+    #     sys.exit(0)
 
-    dir_in = os.getcwd()
-    dir_out = os.getcwd()
-
-    try:
-        dir_in = sys.argv[1]
-        dir_out = sys.argv[2]
-    except:
-        print("Parameters: path/to/simple/file  input/folder  output/folder")
-        sys.exit(0)
-
-    df = pd.read_csv(dir_in)
-    (cal_df, tst_df) = Seperating_data_set(df)
+    #df = pd.read_csv(args.dir_in)
+    df = pd.read_csv(args.input)
+    (cal_df, tst_df) = separating_data_set(df)
 
     (x_train, y_train) = splitting_dataset(cal_df)
     (x_test, y_test) = splitting_dataset(tst_df)
 
-    print(x_train, y_train)
-    print(x_test, y_test)
+    print(x_train)
+    print(y_train)
+    print(x_test)
+    print(y_test)
 
     model = build_fnn(x_train)
 
     model.summary()
     early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=25)
     random.seed(1)
-    model.fit(x_train, y_train, batch_size=20, epochs=400, validation_data=(x_test, y_test), callbacks=[early_stop])
+    model.fit(x_train, y_train, batch_size=args.batchSize, epochs=args.epochSize, validation_data=(x_test, y_test), callbacks=[early_stop])
 
     fnn_losses = pd.DataFrame(model.history.history)
     create_losses_plot(fnn_losses)
