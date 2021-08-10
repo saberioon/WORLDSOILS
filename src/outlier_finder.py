0d2a265a14bfe9cf761c383f47cf6f52ce83721a
saberioon <saberioon@gmail.com>
2021-08-10 16:22:14 +0430

    .gitignore is now working

--- src/outlier_finder.py
+++ src/outlier_finder.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText:`2020 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences Potsdam, Germany'
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: EUPL-1.2
 
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
